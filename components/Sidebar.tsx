0d1b610b05cbe184eccbf8abea4cd14382d803be
AdmiralFirefox <jjlcuadradev@gmail.com>
2023-07-30 15:18:04 +0800

    Added styles for Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,16 +3,40 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { categories } from "@/utils/categories";
+import { IoHomeSharp } from "react-icons/io5";
+import { IconContext } from "react-icons";
+import styles from "@/styles/Sidebar.module.scss";
+
+type CategoryIcon = {
+  children: React.ReactNode;
+};
+
+const CategoryIcon = ({ children }: CategoryIcon) => {
+  return (
+    <IconContext.Provider value={{ className: styles["category-icon"] }}>
+      {children}
+    </IconContext.Provider>
+  );
+};
 
 const Sidebar = () => {
   const pathname = usePathname();
   const isHomePage = pathname === "/";
 
   return (
-    <aside>
+    <aside className={styles["sidebar"]}>
       <ul>
-        <li>
-          <Link href="/" style={{ background: isHomePage ? "red" : "" }}>
+        <li
+          className={
+            isHomePage
+              ? styles["active-category-link"]
+              : styles["category-link"]
+          }
+        >
+          <Link href="/">
+            <CategoryIcon>
+              <IoHomeSharp />
+            </CategoryIcon>
             Home
           </Link>
         </li>
@@ -23,12 +47,19 @@ const Sidebar = () => {
           );
 
           return (
-            <li key={category.id}>
+            <li
+              key={category.id}
+              className={
+                isActive
+                  ? styles["active-category-link"]
+                  : styles["category-link"]
+              }
+            >
               <Link
-                style={{ background: isActive ? "red" : "" }}
                 href={`/category/${category.category_name}`}
                 key={category.category_name}
               >
+                <CategoryIcon>{category.icon}</CategoryIcon>
                 {category.category_name}
               </Link>
             </li>
--- utils/categories.tsx
+++ utils/categories.tsx
@@ -1,21 +1,41 @@
+import { IoTrendingUpSharp } from "react-icons/io5";
+import { IoMusicalNotesSharp } from "react-icons/io5";
+import { BsGearFill } from "react-icons/bs";
+import { BiAtom } from "react-icons/bi";
+import { BiSolidMovie } from "react-icons/bi";
+import { BsPencilFill } from "react-icons/bs";
+import { BiSolidMicrophone } from "react-icons/bi";
+import { IoGameControllerSharp } from "react-icons/io5";
+import { PiCookingPotFill } from "react-icons/pi";
+import { IoNewspaperSharp } from "react-icons/io5";
+import { BsCameraReelsFill } from "react-icons/bs";
+import { IoAmericanFootballSharp } from "react-icons/io5";
+import { GiLargeDress } from "react-icons/gi";
+import { BsFillEmojiLaughingFill } from "react-icons/bs";
+import { GiHealthNormal } from "react-icons/gi";
+import { IoLocationSharp } from "react-icons/io5";
+import { BsFillBrushFill } from "react-icons/bs";
+import { BsFillTreeFill } from "react-icons/bs";
+import { BsFilePlayFill } from "react-icons/bs";
+
 export const categories = [
-  { id: 1, category_name: "Trending", icon: "" },
-  { id: 2, category_name: "Music", icon: "" },
-  { id: 3, category_name: "Technology", icon: "" },
-  { id: 4, category_name: "Science", icon: "" },
-  { id: 5, category_name: "Movies", icon: "" },
-  { id: 6, category_name: "Education", icon: "" },
-  { id: 7, category_name: "Podcast", icon: "" },
-  { id: 8, category_name: "Gaming", icon: "" },
-  { id: 9, category_name: "Cooking", icon: "" },
-  { id: 10, category_name: "News", icon: "" },
-  { id: 11, category_name: "Politics", icon: "" },
-  { id: 12, category_name: "Sports", icon: "" },
-  { id: 13, category_name: "Fashion", icon: "" },
-  { id: 14, category_name: "Comedy", icon: "" },
-  { id: 15, category_name: "Health", icon: "" },
-  { id: 16, category_name: "Travel", icon: "" },
-  { id: 17, category_name: "Art", icon: "" },
-  { id: 18, category_name: "Nature", icon: "" },
-  { id: 19, category_name: "Cars", icon: "" },
+  { id: 1, category_name: "Trending", icon: <IoTrendingUpSharp /> },
+  { id: 2, category_name: "Music", icon: <IoMusicalNotesSharp /> },
+  { id: 3, category_name: "Technology", icon: <BsGearFill /> },
+  { id: 4, category_name: "Science", icon: <BiAtom /> },
+  { id: 5, category_name: "Movies", icon: <BiSolidMovie /> },
+  { id: 6, category_name: "Education", icon: <BsPencilFill /> },
+  { id: 7, category_name: "Podcast", icon: <BiSolidMicrophone /> },
+  { id: 8, category_name: "Gaming", icon: <IoGameControllerSharp /> },
+  { id: 9, category_name: "Cooking", icon: <PiCookingPotFill /> },
+  { id: 10, category_name: "News", icon: <IoNewspaperSharp /> },
+  { id: 11, category_name: "Documentary", icon: <BsCameraReelsFill /> },
+  { id: 12, category_name: "Sports", icon: <IoAmericanFootballSharp /> },
+  { id: 13, category_name: "Fashion", icon: <GiLargeDress /> },
+  { id: 14, category_name: "Comedy", icon: <BsFillEmojiLaughingFill /> },
+  { id: 15, category_name: "Health", icon: <GiHealthNormal /> },
+  { id: 16, category_name: "Travel", icon: <IoLocationSharp /> },
+  { id: 17, category_name: "Art", icon: <BsFillBrushFill /> },
+  { id: 18, category_name: "Nature", icon: <BsFillTreeFill /> },
+  { id: 19, category_name: "Vlogs", icon: <BsFilePlayFill /> },
 ];
