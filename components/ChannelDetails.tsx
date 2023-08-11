4d530744e353e052889f690a906e046242b0c368
AdmiralFirefox <jjlcuadradev@gmail.com>
2023-08-11 18:20:02 +0800

    Fixed undefined channel banners

--- components/ChannelDetails.tsx
+++ components/ChannelDetails.tsx
@@ -4,34 +4,42 @@ import { formatNumber } from "@/utils/formatNumber";
 import { ChannelDetailInfoType } from "@/types/ChannelDetailsType";
 import styles from "@/styles/ChannelDetails.module.scss";
 
-const ChannelDetails = ({ channel }:  ChannelDetailInfoType) => {
+const ChannelDetails = ({ channel }: ChannelDetailInfoType) => {
   const largeScreens = useMediaQuery("(min-width: 30em)");
 
   return (
     <>
-      <div className={styles["banner-wrapper"]}>
-        {largeScreens ? (
-          <Image
-            src={`${channel.brandingSettings.image.bannerExternalUrl}=w2120-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj`}
-            alt=""
-            width={600}
-            height={400}
-            quality={90}
-            unoptimized
-          />
-        ) : (
-          <Image
-            src={`${channel.brandingSettings.image.bannerExternalUrl}=w1440-fcrop64=1,32b75a57cd48a5a8-k-c0xffffffff-no-nd-rj`}
-            alt=""
-            width={600}
-            height={400}
-            quality={90}
-            unoptimized
-          />
-        )}
-      </div>
+      {channel.brandingSettings.image !== undefined ? (
+        <div className={styles["banner-wrapper"]}>
+          {largeScreens ? (
+            <Image
+              src={`${channel.brandingSettings.image.bannerExternalUrl}=w2120-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj`}
+              alt=""
+              width={600}
+              height={400}
+              quality={90}
+              unoptimized
+            />
+          ) : (
+            <Image
+              src={`${channel.brandingSettings.image.bannerExternalUrl}=w1440-fcrop64=1,32b75a57cd48a5a8-k-c0xffffffff-no-nd-rj`}
+              alt=""
+              width={600}
+              height={400}
+              quality={90}
+              unoptimized
+            />
+          )}
+        </div>
+      ) : null}
 
-      <div className={styles["channel-details"]}>
+      <div
+        className={styles["channel-details"]}
+        style={{
+          paddingTop:
+            channel.brandingSettings.image !== undefined ? "1.7em" : "6.5em",
+        }}
+      >
         <div className={styles["channel-image-wrapper"]}>
           <Image
             src={channel.snippet.thumbnails.high.url}
