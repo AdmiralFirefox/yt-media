40f2c22c67bcb1d470c0caa0447fa0a78bc22fa1
AdmiralFirefox <jjlcuadradev@gmail.com>
2023-08-12 22:41:21 +0800

    Adjusted styling and added infinite scroll on suggested videos section

--- components/SuggestedVideos.tsx
+++ components/SuggestedVideos.tsx
@@ -1,15 +1,24 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useDispatch } from "react-redux";
+import { useInfiniteScroll } from "@/hooks/useInfiniteScroll";
 import { v4 as uuidv4 } from "uuid";
 import { setVideoID } from "../features/video/videoSlice";
 import { truncateText } from "@/utils/truncateText";
 import { getTimePassed } from "@/utils/getTimePassed";
 import { SuggestedVideosDescriptionType } from "@/types/SuggestedVideosType";
 import { decode } from "html-entities";
+import LoadingSuggestedSpinner from "./Loaders/LoadingSuggestedSpinner";
 import styles from "@/styles/SuggestedVideos.module.scss";
 
 const SuggestedVideos = ({ videos }: SuggestedVideosDescriptionType) => {
+  const { data, isLoading, loadingRef } = useInfiniteScroll({
+    initialData: videos,
+    pageSize: 5,
+    initialLoadCount: 10,
+    loadingDelay: 1000,
+  });
+
   const router = useRouter();
   const dispatch = useDispatch();
 
@@ -19,39 +28,43 @@ const SuggestedVideos = ({ videos }: SuggestedVideosDescriptionType) => {
   };
 
   return (
-    <ul className={styles["container"]}>
-      {videos
-        .filter((video) => video.id.kind === "youtube#video")
-        .map((video) => (
-          <li
-            key={uuidv4()}
-            onClick={() => getVideoID(video.id.videoId)}
-            className={styles["video-card"]}
-          >
-            <div className={styles["image-container"]}>
-              <Image
-                src={video.snippet.thumbnails.medium.url}
-                alt={video.snippet.title}
-                width={600}
-                height={400}
-                quality={90}
-                unoptimized
-              />
-            </div>
-            <div className={styles["video-details"]}>
-              <p className={styles["video-title"]}>
-                {truncateText(decode(video.snippet.title), 70)}
-              </p>
-              <p className={styles["video-channel-title"]}>
-                {video.snippet.channelTitle}
-              </p>
-              <p className={styles["video-publish-date"]}>
-                {getTimePassed(video.snippet.publishTime)}
-              </p>
-            </div>
-          </li>
-        ))}
-    </ul>
+    <div className={styles["suggestion-wrapper"]}>
+      <ul className={styles["container"]}>
+        {data
+          .filter((video) => video.id.kind === "youtube#video")
+          .map((video) => (
+            <li
+              key={uuidv4()}
+              onClick={() => getVideoID(video.id.videoId)}
+              className={styles["video-card"]}
+            >
+              <div className={styles["image-container"]}>
+                <Image
+                  src={video.snippet.thumbnails.medium.url}
+                  alt={video.snippet.title}
+                  width={600}
+                  height={400}
+                  quality={90}
+                  unoptimized
+                />
+              </div>
+              <div className={styles["video-details"]}>
+                <p className={styles["video-title"]}>
+                  {truncateText(decode(video.snippet.title), 70)}
+                </p>
+                <p className={styles["video-channel-title"]}>
+                  {video.snippet.channelTitle}
+                </p>
+                <p className={styles["video-publish-date"]}>
+                  {getTimePassed(video.snippet.publishTime)}
+                </p>
+              </div>
+            </li>
+          ))}
+      </ul>
+
+      <LoadingSuggestedSpinner isLoading={isLoading} loadingRef={loadingRef} />
+    </div>
   );
 };
 
