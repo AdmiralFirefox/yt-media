7ba2c278d87855db29fee02dd9bb81b7bd80687e
AdmiralFirefox <jjlcuadradev@gmail.com>
2023-08-16 20:43:41 +0800

    Removed order=date param on fetchCategoryData

--- utils/fetchCategoryData.tsx
+++ utils/fetchCategoryData.tsx
@@ -2,7 +2,7 @@ const BASE_URL = "https://youtube.googleapis.com/youtube/v3";
 
 export const fetchCategoryData = async (category: string) => {
   const res = await fetch(
-    `${BASE_URL}/search?part=snippet&maxResults=50&order=date&q=${category}&regionCode=US&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}`,
+    `${BASE_URL}/search?part=snippet&maxResults=50&q=${category}&regionCode=US&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}`,
     { next: { revalidate: 10 } }
   );
 
